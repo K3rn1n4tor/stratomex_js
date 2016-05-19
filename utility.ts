bfe14048b1cbf719356cb67eb7dc3cc7591a49f2
Michael Kern <kernm@in.tum.de>
2016-05-19 17:25:02 +0200

    revised cluster window

--- utility.ts
+++ utility.ts
@@ -10,6 +10,7 @@ import d3 = require('d3');
 import datatypes = require('../caleydo_core/datatype');
 import C = require('../caleydo_core/main');
 import matrix = require('../caleydo_core/matrix');
+import {animationTime} from "./Column";
 
 ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 // class definition of cluster popup
@@ -22,6 +23,8 @@ export class ClusterPopup {
   private $node:d3.Selection<any> = null;
   private destroyed:boolean = false;
   public height:number = 0;
+  private $tabs: d3.Selection<any>[] = [];
+  private $pages: d3.Selection<any>[] = [];
 
   // -------------------------------------------------------------------------------------------------------------------
 
@@ -39,7 +42,7 @@ export class ClusterPopup {
               private options:any) {
     this.options = C.mixin(
       {
-        width: 580,
+        width: 450,
         rowHeight: 35,
         animationTime: 200,
         'kmeans': {
@@ -93,11 +96,6 @@ export class ClusterPopup {
   private _build($parent:d3.Selection<any>, rowID:number) {
     var that = this;
 
-    // find the current rows section in lineup
-    //var $rows = $($parent.node()).find('g.rows')[0];
-    //var $currentRow = $($rows).find('g.row[data-index="' + String(rowID) + '"]')[0];
-    //var $action = $($currentRow).find('tspan[title="cluster"]')[0];
-
     // this works for both Firefox and Chrome
     var mousePos = d3.mouse($parent.node());
 
@@ -109,7 +107,7 @@ export class ClusterPopup {
     // create title
     var $titleBar = $root.append('div').classed('title', true);
     $titleBar.append('i').classed('fa fa-gears', true);
-    $titleBar.append('text').text('Apply Clustering (' + this.data.desc.name + ')').style('margin-left', '5px');
+    $titleBar.append('text').text('Clustering Control Window (' + this.data.desc.name + ')').style('margin-left', '5px');
 
     // create toolbar
     var $toolbar = $root.append('div').classed('toolbar', true);
@@ -119,20 +117,43 @@ export class ClusterPopup {
       });
 
     // create body
+    this.height = 200;
+
     var $body = $root.append('div').classed('body', true);
-    $body.transition().duration(this.options.animationTime).style('width', String(this.options.width) + 'px');
+    $body.transition().duration(this.options.animationTime).style('width', String(this.options.width) + 'px')
+      .style('height', String(this.height) + 'px');
+
+    var $header = $body.append('div').attr('class', 'header');
+
+    var algorithms = ['k-Means', 'Hierarchical', 'Affinity Prop.', 'Fuzzy c-Means'];
+    const numTabs = algorithms.length;
+
+    function onClickTab(index: number) {
+      return () => {
+        for (var j = 0; j < numTabs; ++j)
+        {
+          that.$tabs[j].classed('active', j === index);
+          that.$pages[j].classed('hidden', !(j === index));
+        }
+      }
+    }
+
+    for (var i = 0; i < numTabs; ++i) {
+      var $tab = $header.append('label').attr('class', 'tab').text(algorithms[i]);
+      this.$tabs.push($tab);
+      $tab.on('click', onClickTab(i));
+    }
 
     // create k-means row
-    this._buildKMeansRow($body);
-
+    this._buildKMeansContent($body);
     // create hierarchical row
-    this._buildHierarchicalRow($body);
-
+    this._buildHierarchicalContent($body);
     // create affinity row
-    this._buildAffinityRow($body);
-
+    this._buildAffinityContent($body);
     // create fuzzy row
-    this._buildFuzzyRow($body);
+    this._buildFuzzyContent($body);
+
+    onClickTab(0)();
 
     // use custom offsets
     const offsetX = 5;
@@ -149,66 +170,121 @@ export class ClusterPopup {
 
   // -------------------------------------------------------------------------------------------------------------------
 
-  private _buildKMeansRow($body:d3.Selection<any>) {
+  private _buildKMeansContent($body:d3.Selection<any>) {
     var that = this;
-    this.height += this.options.rowHeight;
 
-    var row = $body.append('div').classed('method', true);
-    var button = row.append('button').text('k-Means');
-    var inputK = row.append('input').attr({
-      'class': 'k-number', type: 'number',
+    var $page = $body.append('div').attr('class', 'tab-content page-kmeans hidden');
+    this.$pages.push($page);
+
+    // ----------------------------------
+
+    var $paramK = $page.append('div').attr('class', 'parameter');
+
+    $paramK.append('label').text('Number of Clusters:');
+
+    var inputK = $paramK.append('input').attr({'class': 'k-number', type: 'number',
       min: this.options.general.numClusters[0], max: this.options.general.numClusters[1],
-      value: this.options.general.numClusters[2], step: 1, title: 'Number of Clusters'
+      value: this.options.general.numClusters[2], step: 1,
+      title: 'Number (k-Value) of disjoint clusters computed by k-Means'
     });
 
-    var inputInit = row.append('select').attr({title: 'Initialization Method'});
+    // ----------------------------------
+
+    var $paramInit = $page.append('div').attr('class', 'parameter');
+
+    $paramInit.append('label').text('Initialization Method:');
+
+    var inputInit = $paramInit.append('select').attr({title: 'Method to choose initial cluster centroids'});
     inputInit.selectAll('option').data(this.options.kmeans.inits)
       .enter().append('option').attr('value', (d:any) => {
       return d;
-    })
-      .text((d:string) => d);
+    }).text((d:string) => d);
 
     inputInit.property('value', this.options.kmeans.inits[this.options.kmeans.initSelect]);
+
+    // ----------------------------------
+
+    var $paramMetric = $page.append('div').attr('class', 'parameter');
+
+    $paramMetric.append('label').text('Distance Metric:');
+
+    var inputDistance = $paramMetric.append('select')
+      .attr({title: 'Distance metric to measure similarity between two records'}).classed('dist', true);
+    inputDistance.selectAll('option').data(this.options.general.distances)
+      .enter().append('option').attr('value', (d:any) => {
+      return d;
+    }).text((d:string) => d);
+
+    // ----------------------------------
+
+    var $controlRun = $page.append('div').attr('class', 'control');
+    var button = $controlRun.append('button').text('Apply Clustering');
 
     button.on('mouseup', (_:any) => {
       const k = parseInt($(inputK.node()).val(), 10);
       const initMethod = $(inputInit.node()).val();
+      const dist = $(inputDistance.node()).val();
 
-      that.stratomex.clusterData(that.data, 'k-means', [k, initMethod]);
+      that.stratomex.clusterData(that.data, 'k-means', [k, initMethod, dist]);
     });
   }
 
   // -------------------------------------------------------------------------------------------------------------------
 
-  private _buildHierarchicalRow($body:d3.Selection<any>) {
+  private _buildHierarchicalContent($body:d3.Selection<any>) {
     var that = this;
-    this.height += this.options.rowHeight;
 
-    var row = $body.append('div').classed('method', true);
-    var button = row.append('button').text('Hierarchical');
-    var inputClusters = row.append('input').attr({
+    var $page = $body.append('div').attr('class', 'tab-content page-hierarchical hidden');
+    this.$pages.push($page);
+
+    // ----------------------------------
+
+    var $paramNum = $page.append('div').attr('class', 'parameter');
+
+    $paramNum.append('label').text('Number of Clusters:');
+
+    var inputClusters = $paramNum.append('input').attr({
       'class': 'k-number', type: 'number',
       min: this.options.general.numClusters[0], max: this.options.general.numClusters[1],
-      value: this.options.general.numClusters[2], step: 1, title: 'Number of Clusters'
+      value: this.options.general.numClusters[2], step: 1, title:
+        'Number of disjoint clusters after cutting the generated dendrogram tree'
     });
 
-    var inputLinkage = row.append('select').attr({title: 'Linkage Method'}).classed('linkage', true);
+    // ----------------------------------
+
+    var $paramLinkage = $page.append('div').attr('class', 'parameter');
+
+    $paramLinkage.append('label').text('Linkage Method:');
+
+    var inputLinkage = $paramLinkage.append('select')
+      .attr({title: 'Method to link two clusters (compute similarity) when building the dendrogram'})
+      .classed('linkage', true);
     inputLinkage.selectAll('option').data(this.options.hierarchical.methods)
       .enter().append('option').attr('value', (d:any) => {
       return d;
-    })
-      .text((d:string) => d);
+    }).text((d:string) => d);
 
     inputLinkage.property('value', this.options.hierarchical.methods[this.options.hierarchical.methodSelect]);
 
-    var inputDistance = row.append('select').attr({title: 'Distance Measurement'}).classed('dist', true);
+    // ----------------------------------
+
+    var $paramMetric = $page.append('div').attr('class', 'parameter');
+
+    $paramMetric.append('label').text('Similarity Metric:');
+
+    var inputDistance = $paramMetric.append('select')
+      .attr({title: 'Distance metric to measure similarity between two records'}).classed('dist', true);
     inputDistance.selectAll('option').data(this.options.general.distances)
       .enter().append('option').attr('value', (d:any) => {
       return d;
-    })
-      .text((d:string) => d);
+    }).text((d:string) => d);
 
     inputDistance.property('value', this.options.general.distances[this.options.hierarchical.distSelect]);
+
+    // ----------------------------------
+
+    var $controlRun = $page.append('div').attr('class', 'control');
+    var button = $controlRun.append('button').text('Apply Clustering');
 
     button.on('mouseup', (_:any) => {
       const k = parseInt($(inputClusters.node()).val(), 10);
@@ -221,40 +297,72 @@ export class ClusterPopup {
 
   // -------------------------------------------------------------------------------------------------------------------
 
-  private _buildAffinityRow($body:d3.Selection<any>) {
+  private _buildAffinityContent($body:d3.Selection<any>) {
     var that = this;
-    this.height += this.options.rowHeight;
 
-    var row = $body.append('div').classed('method', true);
-    var button = row.append('button').text('Affinity');
-    var inputDamping = row.append('input').attr({
-      'class': 'aff-number', type: 'number', name: 'damping',
-      min: this.options.affinity.rangeDamping[0], max: this.options.affinity.rangeDamping[1],
-      value: 0.5, step: 0.05, title: 'Damping Value'
-    });
-    var inputFactor = row.append('input').attr({
-      'class': 'aff-number', type: 'number', name: 'factor',
-      min: this.options.affinity.rangeFactor[0], max: this.options.affinity.rangeFactor[1],
-      value: this.options.affinity.rangeFactor[2], step: 0.05, title: 'Factor Value'
-    });
+    var $page = $body.append('div').attr('class', 'tab-content page-affinity hidden');
+    this.$pages.push($page);
 
-    var inputSelect = row.append('select').attr({title: 'Initial Preference'}).classed('init', true);
+    // ----------------------------------
+
+    var $paramPref = $page.append('div').attr('class', 'parameter');
+
+    $paramPref.append('label').text('Initial Preference Method:');
+
+    var inputSelect = $paramPref.append('select')
+      .attr({title: 'Initial preference for all records (minimum value of similarities creates a small number of ' +
+      'clusters, the median of similarities generates an ordinary number of clusters'}).classed('init', true);
     inputSelect.selectAll('option').data(this.options.affinity.prefs)
       .enter().append('option').attr('value', (d:any) => {
       return d;
-    })
-      .text((d:string) => d);
+    }).text((d:string) => d);
 
     inputSelect.property('value', this.options.affinity.prefs[this.options.affinity.prefSelect]);
 
-    var inputDistance = row.append('select').attr({title: 'Distance Measurement'}).classed('dist', true);
+    // ----------------------------------
+
+    var $paramFactor = $page.append('div').attr('class', 'parameter');
+
+    $paramFactor.append('label').text('Factor of Preferences:');
+
+    var inputFactor = $paramFactor.append('input').attr({
+      'class': 'aff-number', type: 'number', name: 'factor',
+      min: this.options.affinity.rangeFactor[0], max: this.options.affinity.rangeFactor[1],
+      value: this.options.affinity.rangeFactor[2], step: 0.05,
+      title: 'Multiplies the preference value by a factor (also influences the produced number of clusters)'
+    });
+
+    // ----------------------------------
+
+    var $paramDamping = $page.append('div').attr('class', 'parameter');
+
+    $paramDamping.append('label').text('Damping Value:');
+
+    var inputDamping = $paramDamping.append('input').attr({
+      'class': 'aff-number', type: 'number', name: 'damping',
+      min: this.options.affinity.rangeDamping[0], max: this.options.affinity.rangeDamping[1],
+      value: 0.5, step: 0.05, title: 'Dampens updated values after each iteration step to avoid oscillations'
+    });
+
+    // ----------------------------------
+
+    var $paramMetric = $page.append('div').attr('class', 'parameter');
+
+    $paramMetric.append('label').text('Similarity Metric:');
+
+    var inputDistance = $paramMetric.append('select')
+      .attr({title: 'Distance metric to measure similarity between two records'}).classed('dist', true);
     inputDistance.selectAll('option').data(this.options.general.distances)
       .enter().append('option').attr('value', (d:any) => {
       return d;
-    })
-      .text((d:string) => d);
+    }).text((d:string) => d);
 
     inputDistance.property('value', this.options.general.distances[this.options.affinity.distSelect]);
+
+    // ----------------------------------
+
+    var $controlRun = $page.append('div').attr('class', 'control');
+    var button = $controlRun.append('button').text('Apply Clustering');
 
     button.on('mouseup', (_:any) => {
       const affDamping = parseFloat($(inputDamping.node()).val());
@@ -268,29 +376,35 @@ export class ClusterPopup {
 
   // -------------------------------------------------------------------------------------------------------------------
 
-  private _buildFuzzyRow($body:d3.Selection<any>) {
+  private _buildFuzzyContent($body:d3.Selection<any>) {
     var that = this;
-    this.height += this.options.rowHeight;
 
-    var row = $body.append('div').classed('method', true);
-    var button = row.append('button').text('Fuzzy');
+    var $page = $body.append('div').attr('class', 'tab-content page-fuzzy hidden');
+    this.$pages.push($page);
 
-    var inputC = row.append('input').attr({
+    // ----------------------------------
+
+    var $paramC = $page.append('div').attr('class', 'parameter');
+
+    $paramC.append('label').text('Number of Clusters:');
+
+    var inputC = $paramC.append('input').attr({
       'class': 'c-number', type: 'number', name: 'c-number',
       min: this.options.general.numClusters[0], max: this.options.general.numClusters[1],
-      value: this.options.general.numClusters[2], step: 1, title: 'Number of Clusters'
+      value: this.options.general.numClusters[2], step: 1, title: 'Number of fuzzy clusters'
     });
 
-    var inputT = row.append('input').attr({
+    // ----------------------------------
+
+    var $paramThres = $page.append('div').attr('class', 'parameter');
+
+    $paramThres.append('label').text('Probability Threshold:');
+
+    var inputT = $paramThres.append('input').attr({
       'class': 'threshold', type: 'number', name: 'threshold',
       min: this.options.fuzzy.threshold[0], max: this.options.fuzzy.threshold[1],
-      value: 0.2, step: 0.01, title: 'Probability Threshold'
-    });
-
-    var inputM = row.append('input').attr({
-      'class': 'fuzzifier', type: 'number', name: 'fuzzifier',
-      min: this.options.fuzzy.fuzzifier[0], max: this.options.fuzzy.fuzzifier[1],
-      value: this.options.fuzzy.fuzzifier[2], step: 0.001, title: 'Fuzzifier Factor'
+      value: 0.2, step: 0.01,
+      title: 'All records with a probability above or equal this threshold are assigned to a cluster'
     });
 
     function onClusterChange() {
@@ -308,12 +422,47 @@ export class ClusterPopup {
     inputC.on('mouseup', onClusterChange);
     onClusterChange();
 
+    // ----------------------------------
+
+    var $paramFuzzy = $page.append('div').attr('class', 'parameter');
+
+    $paramFuzzy.append('label').text('Fuzziness Factor:');
+
+     var inputM = $paramFuzzy.append('input').attr({
+      'class': 'fuzzifier', type: 'number', name: 'fuzzifier',
+      min: this.options.fuzzy.fuzzifier[0], max: this.options.fuzzy.fuzzifier[1],
+      value: this.options.fuzzy.fuzzifier[2], step: 0.001,
+       title: 'The fuzzifier determines the degree of fuzziness in each cluster.' +
+     ' A value close to 1 creates nearly disjoint clusters, whereas greater values produce more fuzzy (random) clusters'
+    });
+
+    // ----------------------------------
+
+    var $paramMetric = $page.append('div').attr('class', 'parameter');
+
+    $paramMetric.append('label').text('Distance Metric:');
+
+    var inputDistance = $paramMetric.append('select')
+      .attr({title: 'Distance metric to measure similarity between two records'}).classed('dist', true);
+    inputDistance.selectAll('option').data(this.options.general.distances)
+      .enter().append('option').attr('value', (d:any) => {
+      return d;
+    }).text((d:string) => d);
+
+    inputDistance.property('value', this.options.general.distances[this.options.affinity.distSelect]);
+
+    // ----------------------------------
+
+    var $controlRun = $page.append('div').attr('class', 'control');
+    var button = $controlRun.append('button').text('Apply Clustering');
+
     button.on('mouseup', (_:any) => {
       const c = parseInt($(inputC.node()).val(), 10);
       const m = parseFloat($(inputM.node()).val());
       const t = parseFloat($(inputT.node()).val());
+      const dist = $(inputDistance.node()).val();
 
-      that.stratomex.clusterData(that.data, 'fuzzy', [c, m, t]);
+      that.stratomex.clusterData(that.data, 'fuzzy', [c, m, t, dist]);
     });
   }
 
